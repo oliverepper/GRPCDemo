0c22defe3c410e64c398a728e8881fa61c758e88
Oliver Epper <oliver.epper@gmail.com>
2021-06-04 09:01:34 +0200

    more Tests

--- Packages/Server/Tests/ServerTests/ServerTests.swift
+++ Packages/Server/Tests/ServerTests/ServerTests.swift
@@ -103,4 +103,43 @@ final class ServerTests: XCTestCase {
             $0.error = .couldNotStop
         }
     }
+
+    func testCreateSubscription() {
+        let scheduler = DispatchQueue.test
+
+        let environment = ServerEnvironment(
+            mainQueue: scheduler.eraseToAnyScheduler(),
+            createSubscription: { _ in
+                Effect(value: ServerAction.subscriptionCreated)
+            })
+
+        let store = TestStore(initialState: ServerState(),
+                              reducer: serverReducer,
+                              environment: environment)
+
+        store.send(.createSubscriptionBtnTapped)
+
+        scheduler.advance()
+
+        store.receive(.subscriptionCreated)
+    }
+
+    func testReceiveMessageFlow() {
+        let store = TestStore(initialState: ServerState(),
+                              reducer: serverReducer,
+                              environment: ServerEnvironment())
+
+        let message = UUID().uuidString
+
+        store.send(.messageReceived(message)) {
+            $0.alert = .init(
+                title: .init("Message Received"),
+                message: TextState(message),
+                dismissButton: .default(TextState("Ok"), send: .alertDismissed(message)))
+        }
+
+        store.send(.alertDismissed(message)) {
+            $0.receivedMessage = message
+        }
+    }
 }
